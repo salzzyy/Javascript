a89e9cd5af6262f6d521a19f052b110c2a1a79c1
SALONI SINGH <99720223+salzzyy@users.noreply.github.com>
2025-05-26 03:12:29 +0000

    comparisions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+console.log(2>2)
+console.log(2>=1)
+console.log(2>1)
+console.log(2!= 1)
+
+
+
+console.log("2" >1);
+console.log("02" >1);
+
+console.log(null>0);  //false
+console.log(null==0);  //fasle
+console.log(null>= 0);  //true
+
+console.log(undefined ==0)
+console.log(undefined >0)
+console.log(undefined < 0)
+
+// strict check -> === (also check if data type is similar or not, no conversion occurs)
+console.log("2" ===2)  //false
